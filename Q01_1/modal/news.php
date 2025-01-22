5bbb0b4fd50d5fda529d1b76fbd425881a8587d4
Manda <mandashen@gmail.com>
2025-01-22 23:52:31 +0800

    完成news後端功能

--- Q01_1/modal/news.php
+++ Q01_1/modal/news.php
@@ -1,10 +1,10 @@
-<h3 class="cent">新增動態文字廣告</h3>
+<h3 class="cent">新增最新消息資料</h3>
 <hr>
 <form action="api/insert.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
-            <td>動態文字廣告：</td>
-            <td><input type="text" name="text" id="text"></td>
+            <td>最新消息資料：</td>
+            <td><textarea name="text" style="width:300px;height:100px;"></textarea></td>
         </tr>
     </table>
     <div class="cent">
