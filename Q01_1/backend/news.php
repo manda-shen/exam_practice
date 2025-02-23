26edefd69b829cfd1966881b361aecfdb6062ffd
Manda <mandashen@gmail.com>
2025-02-23 17:02:06 +0800

    完成動畫圖片JS控制

--- Q01_1/backend/news.php
+++ Q01_1/backend/news.php
@@ -13,7 +13,7 @@
     </table>
     <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
         <p class="t cent botli">最新消息資料管理</p>
-        <form method="post" target="back" action="./api/edit.php">
+        <form method="post" action="./api/edit.php">
             <table width="100%">
                 <tbody>
                     <tr class="yel">
